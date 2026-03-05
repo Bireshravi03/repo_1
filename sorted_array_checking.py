fada42d7e97bc8d0a0b4bb3774afe186fcf01103
Biresh <fedrickbiresh03@gmail.com>
2026-03-05 12:28:55 +0530

    sorted_array

---
+++ sorted_array_checking.py
@@ -0,0 +1,11 @@
+def brute(arr):
+    n = len(arr)
+    for i in range(n-1):
+        if (arr[i] > arr[i+1]):
+            return False
+    return True
+            
+
+
+arr = [1,2,3,4,5]
+print("Sorted Array :-", brute(arr) )
